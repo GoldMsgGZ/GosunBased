f9073bee53d3155ffbdc5f0260433f689f6ff8d7
baggiowangyu <baggiowangyu@gmail.com>
2018-07-02 20:41:31 +0800

    增加视音频处理

---
+++ src/GxxGmAudioTranscode/GxxGmAudioTranscode.cpp
@@ -0,0 +1,23 @@
+// GxxGmAudioTranscode.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+#include "libswresample/swresample.h"
+#include "libavutil/avutil.h"
+#ifdef __cplusplus
+};
+#endif
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AVSampleFormat 
+	return 0;
+}
+
